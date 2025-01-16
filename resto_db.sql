fe209906b6d9fb69b9dc28fa1bd347fe099afb6f
AlterFM <yogawindy98@gmail.com>
2025-01-16 13:39:00 +0700

    update database

--- resto_db.sql
+++ resto_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 15 Jan 2025 pada 16.34
+-- Waktu pembuatan: 16 Jan 2025 pada 07.37
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -24,22 +24,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `detail_pembelian`
+--
+
+CREATE TABLE `detail_pembelian` (
+  `id_detail` int(11) NOT NULL,
+  `id_pembelian` int(11) DEFAULT NULL,
+  `id_makanan` int(11) DEFAULT NULL,
+  `id_minuman` int(11) DEFAULT NULL,
+  `kuantitas` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `detail_pembelian`
+--
+
+INSERT INTO `detail_pembelian` (`id_detail`, `id_pembelian`, `id_makanan`, `id_minuman`, `kuantitas`, `harga`) VALUES
+(11, 4, NULL, NULL, 1, 15000),
+(12, 4, NULL, NULL, 1, 5000);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `makanan`
 --
 
 CREATE TABLE `makanan` (
   `id_makanan` int(11) NOT NULL,
   `nama_makanan` varchar(50) NOT NULL,
-  `harga_makanan` int(50) NOT NULL
+  `harga_makanan` int(50) NOT NULL,
+  `kode_makanan` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `makanan`
 --
 
-INSERT INTO `makanan` (`id_makanan`, `nama_makanan`, `harga_makanan`) VALUES
-(1, 'Nasi Goreng', 15000),
-(2, 'Mie Goreng', 12000);
+INSERT INTO `makanan` (`id_makanan`, `nama_makanan`, `harga_makanan`, `kode_makanan`) VALUES
+(1, 'Nasi Goreng', 15000, 'M1'),
+(2, 'Mie Goreng', 12000, 'M2'),
+(3, 'Nasi Padang', 25000, 'M3'),
+(4, 'Sop Buntut', 35000, 'M4'),
+(5, 'Ikan Bakar', 30000, 'M5'),
+(6, 'Ayam Bakar Taliwang', 28000, 'M6'),
+(7, 'Sate Kambing', 27000, 'M7'),
+(8, 'Rendang Daging', 35000, 'M8'),
+(9, 'Gulai Kambing', 32000, 'M9'),
+(10, 'Pindang Patin', 30000, 'M10');
 
 -- --------------------------------------------------------
 
@@ -51,16 +83,27 @@ CREATE TABLE `minuman` (
   `id_minuman` int(11) NOT NULL,
   `nama_minuman` varchar(50) NOT NULL,
   `jenis` enum('Dingin','Hangat','Normal') NOT NULL,
-  `harga_minuman` int(11) NOT NULL
+  `harga_minuman` int(11) NOT NULL,
+  `kode_minuman` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `minuman`
 --
 
-INSERT INTO `minuman` (`id_minuman`, `nama_minuman`, `jenis`, `harga_minuman`) VALUES
-(1, 'Es Teh', 'Dingin', 5000),
-(2, 'Kopi', 'Hangat', 10000);
+INSERT INTO `minuman` (`id_minuman`, `nama_minuman`, `jenis`, `harga_minuman`, `kode_minuman`) VALUES
+(1, 'Es Teh', 'Dingin', 5000, 'B1'),
+(2, 'Kopi', 'Hangat', 10000, 'B2'),
+(3, 'Jus Apel', 'Dingin', 10000, 'B3'),
+(4, 'Soda Gembira', 'Dingin', 8000, 'B4'),
+(5, 'Teh Hijau', 'Hangat', 12000, 'B5'),
+(6, 'Milo Hangat', 'Hangat', 14000, 'B6'),
+(7, 'Jus Mangga', 'Normal', 11000, 'B7'),
+(8, 'Es Jeruk', 'Dingin', 9000, 'B8'),
+(9, 'Wedang Jahe', 'Hangat', 13000, 'B9'),
+(10, 'Jus Kiwi', 'Normal', 13000, 'B10'),
+(11, 'Air Mineral', 'Normal', 3000, 'B11'),
+(12, 'Es Cendol', 'Dingin', 9500, 'B12');
 
 -- --------------------------------------------------------
 
@@ -70,17 +113,28 @@ INSERT INTO `minuman` (`id_minuman`, `nama_minuman`, `jenis`, `harga_minuman`) V
 
 CREATE TABLE `pembelian` (
   `id_pembelian` int(11) NOT NULL,
-  `id_makanan` int(11) NOT NULL,
-  `id_minuman` int(11) NOT NULL,
   `nama_pembeli` varchar(50) NOT NULL,
-  `pesanan` varchar(50) NOT NULL,
-  `kuantitas` int(50) NOT NULL,
-  `total_pesanan` int(50) NOT NULL
+  `total_pesanan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pembelian`
+--
+
+INSERT INTO `pembelian` (`id_pembelian`, `nama_pembeli`, `total_pesanan`) VALUES
+(3, 'Basuki', '0'),
+(4, 'Dom', '20000');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_pembelian` (`id_pembelian`);
 
 --
 -- Indeks untuk tabel `makanan`
@@ -98,42 +152,45 @@ ALTER TABLE `minuman`
 -- Indeks untuk tabel `pembelian`
 --
 ALTER TABLE `pembelian`
-  ADD PRIMARY KEY (`id_pembelian`),
-  ADD KEY `fk_pembelian_makanan` (`id_makanan`),
-  ADD KEY `fk_pembelian_minuman` (`id_minuman`);
+  ADD PRIMARY KEY (`id_pembelian`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT untuk tabel `makanan`
 --
 ALTER TABLE `makanan`
-  MODIFY `id_makanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_makanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `minuman`
 --
 ALTER TABLE `minuman`
-  MODIFY `id_minuman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_minuman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `pembelian`
 --
 ALTER TABLE `pembelian`
-  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `pembelian`
+-- Ketidakleluasaan untuk tabel `detail_pembelian`
 --
-ALTER TABLE `pembelian`
-  ADD CONSTRAINT `fk_pembelian_makanan` FOREIGN KEY (`id_makanan`) REFERENCES `makanan` (`id_makanan`),
-  ADD CONSTRAINT `fk_pembelian_minuman` FOREIGN KEY (`id_minuman`) REFERENCES `minuman` (`id_minuman`);
+ALTER TABLE `detail_pembelian`
+  ADD CONSTRAINT `detail_pembelian_ibfk_1` FOREIGN KEY (`id_pembelian`) REFERENCES `pembelian` (`id_pembelian`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
